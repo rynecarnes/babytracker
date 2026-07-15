f89f239a42c3dbff3efc008e1c59d4d485b1c9e7
rynecarnes <ryne.carnes@gmail.com>
2026-07-15 11:30:54 -0400

    adds values to environment properties files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'YOUR_SUPABASE_PROJECT_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  supabaseUrl: 'https://fqnkfiuaogywvoyljgve.supabase.co/rest/v1/',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxbmtmaXVhb2d5d3ZveWxqZ3ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNDE0MzYsImV4cCI6MjA5OTYxNzQzNn0.--DGHYPHXZZqB3Nwkz43TXo8ksjzHFaLiQeiCkoJIZM',
 };
